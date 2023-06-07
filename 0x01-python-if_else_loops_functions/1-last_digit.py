43f31be8b1242844afaadc0a54f9aa47e3c6a60d
amine1 <medaminebouali1@gmail.com>
2023-06-07 01:12:36 +0000

    task 1 to 4

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,9 @@
+mport random
+number = random.randint(-10000, 10000)
+unit = abs(number) % 10
+if number > 5:
+    print(f'Last digit of {number} is {unit} and is greater than 5')
+elif number == 0:
+    print(f'Last digit of {number} is {unit} and is 0')
+else:
+    print(f'Last digit of {number} is -{unit} and is less than 6 and not 0')
