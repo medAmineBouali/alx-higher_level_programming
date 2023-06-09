2362455a97e9b512623631b8ecdc205bf88afc79
medAmineBouali <93989046+medAmineBouali@users.noreply.github.com>
2023-06-09 12:40:18 +0100

    task

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import sys
+from .calculator_1 import (add,sub,mul,div)
 a = int(sys.argv[0])
 operator = sys.argv[1]
 b = int(sys.argv[2])
@@ -8,10 +9,10 @@ if sys.argv.__len__ != 3:
 if operator not in {'+','-','*','/'}:
     print("Unknown operator. Available operators: +, -, * and /")
 elif operator == '+':
-    print("{} {} {} = {}".format(a,operator,b,a + b))
+    print("{} {} {} = {}".format(a,operator,b,add(a,b)))
 elif operator == '-':
-    print("{} {} {} = {}".format(a,operator,b,a - b))
+    print("{} {} {} = {}".format(a,operator,b,sub(a,b)))
 elif operator == '*':
-    print("{} {} {} = {}".format(a,operator,b,a * b))
+    print("{} {} {} = {}".format(a,operator,b,mul(a,b)))
 elif operator == '/':
-    print("{} {} {} = {}".format(a,operator,b,a / b))
+    print("{} {} {} = {}".format(a,operator,b,div(a,b)))
