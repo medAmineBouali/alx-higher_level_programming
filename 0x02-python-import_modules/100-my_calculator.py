e069f8c9bfa429964365ff2dbbd90a134cc5499a
amine1 <medaminebouali1@gmail.com>
2023-06-12 17:52:51 +0000

    task 6

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import sys
 if __name__ == "__main__":
-    from calculator_1 import *
+    from calculator_1 import (add,sub,mul,div)
 if len(sys.argv) != 4:
     print("Usage: ./100-my_calculator.py <a> <operator> <b>")
     exit(1)
