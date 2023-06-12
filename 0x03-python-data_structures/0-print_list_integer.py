51173444fdbeec3f2875423147c15128174d3755
medAmineBouali <93989046+medAmineBouali@users.noreply.github.com>
2023-06-12 20:13:02 +0100

    task

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
     for i in my_list:
-        if i == len(my_list):
-            print("{:d}".format(i), end='')
-        else:
-            print("{:d}".format(i))
+        print("{:d}".format(i))
