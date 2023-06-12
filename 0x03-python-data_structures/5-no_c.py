ae6ac6ac8996f286134d3eb2d1710a17a4748e28
medAmineBouali <93989046+medAmineBouali@users.noreply.github.com>
2023-06-12 23:37:35 +0100

    task

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,8 +3,6 @@ def no_c(my_string):
     if my_string:
         new_string = ""
         for c in my_string:
-            if c != 'c':
+            if c != 'c' and c != 'C':
                 new_string += c
-            else:
-                new_string += 'C'
         return new_string
