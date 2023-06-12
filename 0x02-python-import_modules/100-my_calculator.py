47421b37c28a8e8934039ef96e68c38ad56c0772
amine1 <medaminebouali1@gmail.com>
2023-06-12 17:45:31 +0000

    task 6

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,14 +2,15 @@
 import sys
 if __name__ == "__main__":
     from calculator_1 import *
-a = int(sys.argv[1])
-operator = sys.argv[2]
-b = int(sys.argv[3])
 if len(sys.argv) != 4:
     print("Usage: ./100-my_calculator.py <a> <operator> <b>")
     exit(1)
+a = int(sys.argv[1])
+operator = sys.argv[2]
+b = int(sys.argv[3])
 if operator not in ['+','-','*','/']:
     print("Unknown operator. Available operators: +, -, * and /")
+    exit(1)
 elif operator == '+':
     print("{} {} {} = {}".format(a,operator,b,add(a,b)))
 elif operator == '-':
