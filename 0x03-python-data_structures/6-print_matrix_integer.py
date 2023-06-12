4c5d69a24a77d470f2e27d02d1992708a2b7a08d
medAmineBouali <93989046+medAmineBouali@users.noreply.github.com>
2023-06-13 00:04:24 +0100

    task

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,5 +3,5 @@ def print_matrix_integer(matrix=[[]]):
     if matrix:
         for i in range(len(matrix)):
             for j in range(len(matrix[i])):
-                print("{}".format(matrix[i][j]), end=' ')
-            print('$')
+                print("{:d}".format(matrix[i][j]), end=' ')
+            print("")
