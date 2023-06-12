0809c3cfbfe20ec410bbcdb3224d21c6aa32a0ab
amine1 <medaminebouali1@gmail.com>
2023-06-12 17:42:07 +0000

    task 6

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 import sys
 if __name__ == "__main__":
-    from .calculator_1 import *
-a = int(sys.argv[0])
-operator = sys.argv[1]
-b = int(sys.argv[2])
-if sys.argv.__len__ != 3:
+    from calculator_1 import *
+a = int(sys.argv[1])
+operator = sys.argv[2]
+b = int(sys.argv[3])
+if len(sys.argv) != 4:
     print("Usage: ./100-my_calculator.py <a> <operator> <b>")
     exit(1)
 if operator not in ['+','-','*','/']:
