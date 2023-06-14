fad154af29ff21785a3452b7ebe01550070744a5
medAmineBouali <93989046+medAmineBouali@users.noreply.github.com>
2023-06-14 15:37:26 +0100

    task

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    if matrix:
+        new_matrix = [[]]
+        for i in range(len(matrix)):
+            for j in range(len(matrix[i])):
+                new_matrix[i][j] = matrix[i][j] ** 2
+    return new_matrix
