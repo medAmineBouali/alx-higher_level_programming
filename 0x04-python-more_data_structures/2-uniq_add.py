70f22cc0149fa3efa7011bded58d17f53ecc2f3c
medAmineBouali <93989046+medAmineBouali@users.noreply.github.com>
2023-06-14 16:51:38 +0100

    task

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    sum = int(0)
+    for i in range(len(my_list)):
+        if my_list[i] not in my_list[:i]:
+            sum += int(my_list[i])
+    return (sum)
