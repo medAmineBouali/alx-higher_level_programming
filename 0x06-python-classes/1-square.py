44bf6d1ef7a2cd2ee575600f6cdd9deb57b408d4
medAmineBouali <93989046+medAmineBouali@users.noreply.github.com>
2023-06-27 17:52:53 +0100

    task

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -3,14 +3,12 @@
 
 
 class Square:
-    """Represents a square
-    Attributes:
-        __size (int): size of a side of the square
-    """
+    """Represents a square"""
+
     def __init__(self, size=None):
         """Initializes a square
         Args:
-            size (int): size of a side of the square
+            size : size of a side of the square
         Returns: None
         """
         self.__size = int(size)
---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""This a Square class"""
+
+
+class Square:
+    """Represents a square"""
+    
+    def __init__(self, size=0):
+        """Initializes a square
+        Args:
+            size (int): size of a side of the square
+        Returns: None
+        """
+        if (size is not int):
+            raise TypeError("size must be an integer")
+        if (size < 0):
+            raise ValueError("size must be >= 0")
+        self.__size = int(size)
