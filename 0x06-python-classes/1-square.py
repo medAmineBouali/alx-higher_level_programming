ac9cee1de647a97e3c4a00b6acda384f45ad8377
medAmineBouali <93989046+medAmineBouali@users.noreply.github.com>
2023-06-27 16:43:35 +0100

    task

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -3,6 +3,14 @@
 
 
 class Square:
-
+    """Represents a square
+    Attributes:
+        __size (int): size of a side of the square
+    """
     def __init__(self, size=None):
+        """Initializes a square
+        Args:
+            size (int): size of a side of the square
+        Returns: None
+        """
         self.__size = int(size)
