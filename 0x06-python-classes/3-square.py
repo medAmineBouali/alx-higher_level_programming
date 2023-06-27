d548940ff42404e69a68ac377e45309574927d1f
medAmineBouali <93989046+medAmineBouali@users.noreply.github.com>
2023-06-27 18:26:56 +0100

    task

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -11,23 +11,14 @@ class Square:
             size (int): size of a side of the square
         Returns: None
         """
-        self.size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
       
     def area(self):
         """area of square
         Returns: square area
         """
         return (self.__size**2)
-
-    @property
-    def size(self):
-        """Get/set the current size of the square."""
-        return (self.__size)
-
-    @size.setter
-    def size(self, value):
-        if not isinstance(value, int):
-            raise TypeError("size must be an integer")
-        elif value < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = value
