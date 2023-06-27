8db59967d9a03dfb2fc11d9420905a35d012a274
medAmineBouali <93989046+medAmineBouali@users.noreply.github.com>
2023-06-27 17:55:37 +0100

    task

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -15,4 +15,4 @@ class Square:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = int(size)
+        self.__size = size
