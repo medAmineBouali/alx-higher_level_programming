b099a371f4bb53f6bb1c45f4775d3534b5966032
medAmineBouali <93989046+medAmineBouali@users.noreply.github.com>
2023-07-03 17:18:56 +0100

    task

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,9 +6,19 @@ class Rectangle:
     """This defines the properties and methods of rectangles."""
 
     def __init__(self, width=0, height=0):
-        self.width = width
-        self.height = height
-          
+        if type(width) is not int:
+            raise TypeError("Width must be an integer")
+        elif width < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = width
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        elif height <= 0:
+            raise ValueError("height must be > 0")
+        else:
+            self.__height = height
+            
     @property
     def width(self):
         return (self.__width)
