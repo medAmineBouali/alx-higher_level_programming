0d827d482893f59388fe78041feb01880e2045ca
medAmineBouali <93989046+medAmineBouali@users.noreply.github.com>
2023-07-03 16:26:07 +0100

    task

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""this is a rectangle class"""
+
+
+class Rectangle:
+    """This defines the properties and methods of rectangles."""
+
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+          
+    @property
+    def width(self):
+        return (self.__width)
+    
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("Width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+    
+    @property
+    def height(self, value):
+        return (self.__height)
+    
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        elif value <= 0:
+            raise ValueError("height must be > 0")
+        else:
+            self.__height = value
