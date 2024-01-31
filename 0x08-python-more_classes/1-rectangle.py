9840b6bce34ef7a34cb48915c532d6721288ad75
medAmineBouali <93989046+medAmineBouali@users.noreply.github.com>
2024-01-31 21:36:49 +0100

    task

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """This program define a class Rectangle with it attributes"""
+
+
 class Rectangle:
     """
     A Rectangle Class with the private instance attributes width and height
@@ -10,21 +12,28 @@ class Rectangle:
 
     @property
     def width(self):
+        """width property getter"""
+
         return self.__width
     
     @width.setter
     def width(self,value):
+        """width property setter"""
+
         if not isinstance(value,int):
             raise TypeError("width must be an integer")
         if value < 0 :
             raise ValueError("width must be >= 0")
         self.__width = value
+
     @property
     def height(self):
+        """height property getter"""
         return self.__height
     
     @height.setter
     def height(self,value):
+        """height property setter"""
         if not isinstance(value,int):
             raise TypeError("height must be an integer")
         if value < 0 :
