726f7ff94ec716561b5c04263cb859216dc54c20
medAmineBouali <93989046+medAmineBouali@users.noreply.github.com>
2024-02-01 01:39:25 +0100

    task

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -65,3 +65,10 @@ class Rectangle:
     def __del__(self):
         self.instance_tracker(-1)
         print("Bye rectangle...")
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        if not isinstance(rect_1,Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if not isinstance(rect_2,Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        return rect_1 if rect_1.area() >= rect_2.area() else rect_2
