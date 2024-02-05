d10669fca47442f871dc8dc4b0ecf82668f0f1ca
medAmineBouali <93989046+medAmineBouali@users.noreply.github.com>
2024-02-05 23:56:38 +0100

    task

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"""Defines a class Rectangle that inherits from BaseGeometry."""
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+"""Defines a class Rectangle that inherits from BaseGeometry."""
 
 
 class Rectangle(BaseGeometry):
