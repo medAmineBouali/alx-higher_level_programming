452c9cd974effd79641e564e9add82f8e35f4b88
medAmineBouali <93989046+medAmineBouali@users.noreply.github.com>
2024-02-05 23:11:53 +0100

    pythone inheritance

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 """Defines an object attribute lookup function."""
 
-
 def lookup(obj):
     """Return a list of an object's available attributes."""
     return (dir(obj))
+class Find:
+    pass
+me = Find()
+print(lookup(me))
--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """Defines an inherited list class MyList."""
 
-
 class MyList(list):
     """Implements sorted printing for the built-in list class."""
 
