3106cea558df6526e0c9fff013a049a652514269
medAmineBouali <93989046+medAmineBouali@users.noreply.github.com>
2024-02-09 18:35:19 +0100

    task

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -35,7 +35,7 @@ def matrix_divided(matrix, div):
     if not all(len(row) == len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
 
-    if not isinstance(div, int) and not isinstance(div, float):
+    if not isinstance(div, (int,float)):
         raise TypeError("div must be a number")
     
 
